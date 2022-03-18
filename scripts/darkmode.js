e05a46b752a10401d1e18c6d3fef83db2f351c20
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-03-18 13:30:00 +0000

    made contact section responsive

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -3,14 +3,14 @@ checkbox.addEventListener('change', () => {
     const styles = getComputedStyle(document.documentElement);
     const bgColor = styles.getPropertyValue('--bg-color');
     if (bgColor == '#f9fcff') {
-        document.documentElement.style.setProperty('--bg-color', '#121213');
+        document.documentElement.style.setProperty('--bg-color', '#23272a');
         document.documentElement.style.setProperty('--bg-lighter', '#222224');
-        document.documentElement.style.setProperty('--bg-lighter-lighter', '#282828');
+        document.documentElement.style.setProperty('--bg-lighter-lighter', '#2c2f33');
         document.documentElement.style.setProperty('--bg-font', '#dddddd')
     } else {
         document.documentElement.style.setProperty('--bg-color', '#f9fcff');
         document.documentElement.style.setProperty('--bg-lighter', '#dddddd');
         document.documentElement.style.setProperty('--bg-lighter-lighter', '#efefef');
-        document.documentElement.style.setProperty('--bg-font', '#282828')
+        document.documentElement.style.setProperty('--bg-font', '#2c2f33')
     }
 });
